fb9fb17751d86b05a12a110c8c12c6497a2fc4d2
Pipe Gutierrez <luis.gutierrez@konfio.mx>
2017-08-05 17:22:43 -0500

    Filters out empty value arguments

--- src/Commando/Command.php
+++ src/Commando/Command.php
@@ -640,9 +640,17 @@ class Command implements \ArrayAccess, \Iterator
     public function getArgumentValues()
     {
         $this->parseIfNotParsed();
+
+        $arguments = $this->arguments;
+        $arguments = array_filter($arguments, function(Option $argument){
+            $argumentValue = $argument->getValue();
+            return isset($argumentValue);
+        });
+
+
         return array_map(function(Option $argument) {
             return $argument->getValue();
-        }, $this->arguments);
+        }, $arguments);
     }
 
     /**
