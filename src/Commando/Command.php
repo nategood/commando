a58b69af10b19ea1a0d8586ea3ebdf8936cb851f
Laurent VOULLEMIER <laurent.voullemier@gmail.com>
2018-06-14 10:36:34 +0200

    Add short help option

--- src/Commando/Command.php
+++ src/Commando/Command.php
@@ -455,7 +455,7 @@ class Command implements \ArrayAccess, \Iterator
                     $count++;
                 } else {
                     // Short circuit if the help flag was set and we're using default help
-                    if ($this->use_default_help === true && $name === 'help') {
+                    if ($this->use_default_help === true && ($name === 'help' || $name === 'h')) {
                         $this->printHelp();
                         exit;
                     }
@@ -797,6 +797,7 @@ class Command implements \ArrayAccess, \Iterator
     {
         // Add in a default help method
         $this->option('help')
+            ->alias('h')
             ->describe('Show the help page for this command.')
             ->boolean();
     }
